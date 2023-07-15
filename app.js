ffa07f6816fdc4c5a8d0ed15a8a25f3632804dad
Dumitru432 <89398647+Dumitru432@users.noreply.github.com>
2023-07-15 10:07:55 +0300

    exam

---
+++ app.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiperTest", {
+    direction: "vertical",
+    autoplay: {
+        delay: 4000,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
